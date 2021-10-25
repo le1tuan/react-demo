da2806d5df873067cd13b83f417c6aea4fbfd079
snow04019x <ben10as124@gmail.com>
2021-10-24 23:46:18 -0700

    update code

--- src/components/client/ClientPage.js
+++ src/components/client/ClientPage.js
@@ -1,9 +1,11 @@
 import { Container } from 'react-bootstrap';
 import LeftSideClientPage from './LeftSideClientPage';
+import RightSideClientPage from './RightSideClientPage';
 
 const ClientPage = () => (
-  <Container fluid>
+  <Container fluid className="d-flex">
     <LeftSideClientPage />
+    <RightSideClientPage />
   </Container>
 );
 export default ClientPage;
--- src/components/client/RightSideClientPage.js
+++ src/components/client/RightSideClientPage.js
@@ -1,9 +1,21 @@
-const RightSideClientPage =  () => {
-    return (
-        <div>
+import { Row, Col } from 'react-bootstrap';
 
+const RightSideClientPage = () => (
+  <Row className="p-0 right-size-main" style={{ maxWidth: '1680px' }}>
+    <Col xs lg={12} className="p-0">
+      <div className="right-side-header" style={{ height: '64px', backgroundColor: '#2196F3' }}>
+        <div className="header-left">
+          <div>
+            <i className="fas fa-expand" />
+          </div>
         </div>
-    )
-}
+        <div className="header-right">
+          <div>
+            <i className="far fa-paper-plane" />
+          </div>
+        </div>
+      </div>
+    </Col>
+  </Row>
+);
 export default RightSideClientPage;
-
